081aaf63bac78b69558d4e64644919ff7367d410
Varad Dongare <1272250418@mitwpu.edu.in>
2026-02-22 00:43:16 +0530

    fixed executable path

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const client = new Client({
             '--no-zygote',
             '--single-process', 
         ],
-        executablePath: '/opt/render/project/src/.cache/puppeteer/chrome/linux-145.0.7632.77/chrome-linux64/chrome'
+        
     }
 });
 
