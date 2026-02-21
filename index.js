bca403138d43e75dc9334b408aed68ca3bff1c1f
Varad Dongare <1272250418@mitwpu.edu.in>
2026-02-22 00:49:30 +0530

    fixed path

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ const client = new Client({
             '--no-sandbox',
             '--disable-setuid-sandbox',
             '--disable-dev-shm-usage',
-            '--disable-accelerated-2d-canvas',
-            '--no-first-run',
             '--no-zygote',
             '--single-process', 
         ],
